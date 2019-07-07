5ab87000e78a94005fe12e0dafe7582d4908d444
Alexander Stante <stante@gmail.com>
2019-07-07 15:21:28 +0200

    Add generator and discriminator steps in training loop

--- dcgan-mnist.py
+++ dcgan-mnist.py
@@ -3,6 +3,9 @@ import torchvision.datasets.mnist as mnist
 import torch.utils.data.dataloader as dataloader
 import torchvision.transforms as transforms
 import torch
+import torch.optim as optim
+import torch.nn as nn
+import model
 
 
 @click.command()
@@ -16,19 +19,49 @@ def main(root, batch_size, latent_vector, disable_cuda):
     else:
         device = torch.device('cpu')
 
-    transform = transforms.Compose([transforms.ToTensor(),
+    transform = transforms.Compose([transforms.Resize(32),
+                                    transforms.ToTensor(),
                                     transforms.Lambda(lambda x: x * 2 - 1)])
     trainset = mnist.MNIST(root=root, download=True, train=True, transform=transform)
     testset = mnist.MNIST(root=root, download=True, train=False, transform=transform)
     trainloader = dataloader.DataLoader(dataset=trainset, batch_size=batch_size, shuffle=True, drop_last=True)
     testloader = dataloader.DataLoader(dataset=testset, batch_size=batch_size, shuffle=True, drop_last=True)
 
+    generator = model.DCGANModelGenerator(latent_vector)
+    discriminator = model.DCGANModelDiscriminator()
+    gen_optim = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    disc_optim = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    criterion = nn.MSELoss()
+    generator.to(device)
+    discriminator.to(device)
+
     for x_image, y_label in trainloader:
         x_image = x_image.to(device)
-        y_label = y_label.to(device)
+        # y_label = y_label.to(device)
 
-        z = torch.rand((latent_vector, 1), device=device) * 2 - 1
-        pass
+        discriminator.train()
+        generator.eval()
+
+        disc_optim.zero_grad()
+        z = torch.rand((batch_size, latent_vector), device=device) * 2 - 1
+        x_gen = generator.forward(z)
+        out = discriminator.forward(torch.cat([x_image, x_gen]))
+        dloss = criterion(out, torch.cat((torch.ones_like(y_label, dtype=torch.float32, device=device),
+                                          torch.zeros_like(y_label, dtype=torch.float32, device=device))))
+        dloss.backward()
+        disc_optim.step()
+
+        discriminator.eval()
+        generator.train()
+
+        gen_optim.zero_grad()
+        z = torch.rand((batch_size, latent_vector), device=device) * 2 - 1
+        x_gen = generator.forward(z)
+        out = discriminator.forward(x_gen)
+        gloss = criterion(out, torch.ones_like(out, dtype=torch.float32, device=device))
+
+        gloss.backward()
+        gen_optim.step()
 
 
 if __name__ == '__main__':
