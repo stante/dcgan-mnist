939c98d456c221275d00850fc3d0609b763948ba
Alexander Stante <stante@gmail.com>
2019-07-07 17:19:00 +0200

    Reduce capacity of generator

--- model.py
+++ model.py
@@ -8,17 +8,17 @@ class DCGANModelGenerator(nn.Module):
         super(DCGANModelGenerator, self).__init__()
 
         self.latent_dim = latent_dim
-        self.project = nn.Linear(latent_dim, 4 * 4 * 512)
-        self.bn = nn.BatchNorm1d(4 * 4 * 512)
+        self.project = nn.Linear(latent_dim, 4 * 4 * 256)
+        self.bn = nn.BatchNorm1d(4 * 4 * 256)
 
         self.conv = nn.Sequential(
-            nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
             nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2, bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 1, kernel_size=2, stride=2),
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 1, kernel_size=2, stride=2),
             nn.Tanh()
         )
 
