ef3da6ec25de354061a1842314a0cf962065ce2f
Alexander Stante <stante@gmail.com>
2019-07-07 12:22:25 +0200

    Add command line options for root directory and batch size

--- dcgan-mnist.py
+++ dcgan-mnist.py
@@ -6,13 +6,15 @@ import torch
 
 
 @click.command()
-def main():
+@click.option('--root', default="~/.torch/mnist", help="Root directory for MNIST dataset")
+@click.option('--batch-size', default=128, help="Batch size")
+def main(root, batch_size):
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Lambda(lambda x: x * 2 - 1)])
-    trainset = mnist.MNIST(root="~/.torch/mnist", download=True, train=True, transform=transform)
-    testset = mnist.MNIST(root="~/.torch/mnist", download=True, train=False, transform=transform)
-    trainloader = dataloader.DataLoader(dataset=trainset, batch_size=128, shuffle=True, drop_last=True)
-    testloader = dataloader.DataLoader(dataset=testset, batch_size=128, shuffle=True, drop_last=True)
+    trainset = mnist.MNIST(root=root, download=True, train=True, transform=transform)
+    testset = mnist.MNIST(root=root, download=True, train=False, transform=transform)
+    trainloader = dataloader.DataLoader(dataset=trainset, batch_size=batch_size, shuffle=True, drop_last=True)
+    testloader = dataloader.DataLoader(dataset=testset, batch_size=batch_size, shuffle=True, drop_last=True)
 
     for x_image, y_label in trainloader:
         pass
