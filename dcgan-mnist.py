eeb344784b215ad3be4b31b440b33b0a414f70f2
Alexander Stante <stante@gmail.com>
2019-07-07 12:57:48 +0200

    Add CUDA handling

--- dcgan-mnist.py
+++ dcgan-mnist.py
@@ -9,7 +9,13 @@ import torch
 @click.option('--root', default="~/.torch/mnist", help="Root directory for MNIST dataset")
 @click.option('--batch-size', default=128, help="Batch size")
 @click.option('--latent-vector', default=100, help="Size of latent vector Z")
-def main(root, batch_size, latent_vector):
+@click.option('--disable-cuda', default=False, help="Disable CUDA acceleration")
+def main(root, batch_size, latent_vector, disable_cuda):
+    if not disable_cuda and torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Lambda(lambda x: x * 2 - 1)])
     trainset = mnist.MNIST(root=root, download=True, train=True, transform=transform)
@@ -18,7 +24,10 @@ def main(root, batch_size, latent_vector):
     testloader = dataloader.DataLoader(dataset=testset, batch_size=batch_size, shuffle=True, drop_last=True)
 
     for x_image, y_label in trainloader:
-        z = torch.rand((latent_vector, 1)) * 2 - 1
+        x_image = x_image.to(device)
+        y_label = y_label.to(device)
+
+        z = torch.rand((latent_vector, 1), device=device) * 2 - 1
         pass
 
 
