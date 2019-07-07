8a4538551872b877b9f87bc650d45b833e8b17a9
Alexander Stante <stante@gmail.com>
2019-07-07 12:08:24 +0200

    Add loading of MNIST dataset

--- dcgan-mnist.py
+++ dcgan-mnist.py
@@ -1,9 +1,21 @@
 import click
+import torchvision.datasets.mnist as mnist
+import torch.utils.data.dataloader as dataloader
+import torchvision.transforms as transforms
+import torch
 
 
 @click.command()
 def main():
-    pass
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Lambda(lambda x: x * 2 - 1)])
+    trainset = mnist.MNIST(root="~/.torch/mnist", download=True, train=True, transform=transform)
+    testset = mnist.MNIST(root="~/.torch/mnist", download=True, train=False, transform=transform)
+    trainloader = dataloader.DataLoader(dataset=trainset, batch_size=128, shuffle=True, drop_last=True)
+    testloader = dataloader.DataLoader(dataset=testset, batch_size=128, shuffle=True, drop_last=True)
+
+    for x_image, y_label in trainloader:
+        pass
 
 
 if __name__ == '__main__':
