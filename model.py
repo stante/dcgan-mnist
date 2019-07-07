b7961a0ac862cc2626ca39d1d7f169a6a0b99c3e
Alexander Stante <stante@gmail.com>
2019-07-07 11:25:57 +0200

    Add Generator and Discriminator template

--- model.py
+++ model.py
@@ -1,7 +1,15 @@
 import torch.nn as nn
 
 
-class DCGANModel(nn.Module):
+class DCGANModelGenerator(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+
+class DCGANModelDiscriminator(nn.Module):
     def __init__(self):
         pass
 
