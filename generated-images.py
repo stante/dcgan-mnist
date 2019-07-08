c7fc585522c7b77b012e22b27268a5e051f72f19
Alexander Stante <stante@gmail.com>
2019-07-08 21:01:59 +0200

    Define function for writing images to disk

--- generated-images.py
+++ generated-images.py
@@ -2,14 +2,19 @@ import matplotlib.pyplot as plt
 import pickle
 
 
-with open('generated_images.pkl', 'rb') as f:
-    images = pickle.load(f)
+def main():
+    with open('generated_images_01.pkl', 'rb') as f:
+        images = pickle.load(f)
 
-for image in images:
-    fig = plt.figure()
+    for n, image in enumerate(images):
+        fig = plt.figure()
 
-    for i in range(16):
-        fig.add_subplot(4, 4, i + 1)
-        plt.imshow(image[i], cmap='gray')
+        for i in range(16):
+            fig.add_subplot(4, 4, i + 1)
+            plt.imshow(image[i], cmap='gray')
 
-    plt.show()
+        plt.savefig("data/fig_{:03}.png".format(n))
+
+
+if __name__ == '__main__':
+    main()
