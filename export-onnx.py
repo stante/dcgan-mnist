87236534ece24c0f291ddf3af78db1537fe0cb99
Alexander Stante <stante@gmail.com>
2019-07-10 22:20:40 +0200

    Add simple onnx export

---
+++ export-onnx.py
@@ -0,0 +1,21 @@
+import torch
+from model import DCGANModelGenerator
+import click
+
+
+@click.command()
+@click.argument('input-file')
+@click.argument('output-file')
+@click.option('--verbose', default=True)
+def main(input_file, output_file, verbose):
+    model = DCGANModelGenerator(100)
+    model.load_state_dict(torch.load(input_file))
+    model.eval()
+
+    z = torch.rand((1, 100)) * 2 - 1
+
+    torch.onnx.export(model, z, output_file, verbose=verbose)
+
+
+if __name__ == '__main__':
+    main()
